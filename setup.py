0171f4a883e3179a3a9b6af1dd54ce3fe84dfcdd
Eisa <eisashaiju@gmail.com>
2025-10-16 19:51:11 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Eisa Shaiju",
+    author_email="eisashaijugmail.com",
+    packages=find_packages()
+)
